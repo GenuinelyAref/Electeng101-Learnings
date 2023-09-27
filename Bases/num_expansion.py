224601e20c4dbcae259feb5e4b671ab563fd3525
GenuinelyAref <aref.osman18@gmail.com>
2023-09-28 00:58:27 +1300

    v2
    - add extensive commenting
    - clean variable names and code behaviour
    - add number & base writing (hardcoded for now)

--- Bases/num_expansion.py
+++ Bases/num_expansion.py
@@ -1,7 +1,7 @@
 # program to expand out number in terms of its base
 
 # function to retrieve subscript character unicode equivalent for any digit (0-9)
-def special_num(num, var_type, sign):
+def special_num(var_num, var_type, var_sign):
     # define subscript unicode character lists
     if var_type == "sub":
         unicode_chars = ['\u2080', '\u2081', '\u2082', '\u2083', '\u2084',
@@ -14,15 +14,22 @@ def special_num(num, var_type, sign):
     # define used variable
     special_text = ""
     # repeat for each digit in the number provided
-    for i in range(0, len(str(num))):
+    for i in range(0, len(str(var_num))):
         # convert each digit into its subscript unicode equivalent
-        special_text += unicode_chars[int((str(num))[i])]
-    if sign == "-":
+        special_text += unicode_chars[int((str(var_num))[i])]
+    if var_sign == "-":
         special_text = negative_superscript + special_text
     # return subscript unicode equivalent to caller (ready to print)
     return special_text
 
 
+# write number with base in subscript
+def write_number(var_num, var_base):
+    var_return_num = "{}{}".format(var_num, special_num(var_base, "sub", "+"))
+    return var_return_num
+
+
+# expand a single digit using base and place-value
 def expanded_term(var_digit, var_base, var_place_value, var_sign):
     var_expanded_term = "{}x{}{}".format(var_digit, var_base, special_num(var_place_value, "super", var_sign))
     return var_expanded_term
@@ -48,39 +55,49 @@ def full_expansion(var_num, var_base):
     # if number has no decimal part
     if dot_index == -1:
         # go down each digit
-        for digit in range(var_digits - 1, -1, -1):
+        for digit_index in range(0, var_digits):
+            # get power of base
+            var_placeholder = var_digits - digit_index - 1
             # get digit from number
-            var_digit = str_var_num[var_digits - digit - 1]
+            var_digit = str_var_num[digit_index]
             # if it's not the last digit, add a "+" and <space> at the end
-            if digit != 0:
-                expanded_text += "{} + ".format(expanded_term(var_digit, var_base, digit, "+"))
+            if digit_index != var_digits - 1:
+                # expand the digit out using base and place-value
+                expanded_text += "{} + ".format(expanded_term(var_digit, var_base, var_placeholder, "+"))
             # otherwise no space or plus sign
             else:
-                expanded_text += "{}".format(expanded_term(var_digit, var_base, digit, "+"))
+                # expand the digit out using base and place-value
+                expanded_text += "{}".format(expanded_term(var_digit, var_base, var_placeholder, "+"))
     
     # if number has a decimal point
     else:
         # go down each digit
-        for digit in range(0, var_digits):
+        for digit_index in range(0, var_digits):
             # get power of base
-            var_placeholder = dot_index - digit - 1
+            var_placeholder = dot_index - digit_index - 1
             # adjust negative powers to display correctly
             if var_placeholder < 0:
                 var_sign = "-"
                 var_placeholder = var_placeholder * -1
             else:
                 var_sign = "+"
-            # get digit from list
-            var_digit = str_var_num[digit]
+            # get digit from number
+            var_digit = str_var_num[digit_index]
             # if it's not the last digit, add a "+" and <space> at the end
-            if digit != var_digits - 1:
+            if digit_index != var_digits - 1:
+                # expand the digit out using base and place-value
                 expanded_text += "{} + ".format(expanded_term(var_digit, var_base, var_placeholder, var_sign))
             # otherwise no space or plus sign
             else:
+                # expand the digit out using base and place-value
                 expanded_text += "{}".format(expanded_term(var_digit, var_base, var_placeholder, var_sign))
 
+    # return expanded form of number
     return expanded_text
 
 
-f = full_expansion(9365.4128376, 10)
-print(f)
+number = 92.374
+base = 10
+w = write_number(number, base)
+f = full_expansion(number, base)
+print("{} = {}".format(w, f))
